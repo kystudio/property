183e1e1d849ca62f11899f50f497f9c7bff9c7fc
袁麟福 <kylin_studio@icloud.com>
2018-04-03 07:37:41 +0800

    Initial Commit

---
+++ property/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  property
+//
+//  Created by kotlin on 2018/4/3.
+//  Copyright © 2018年 kotlin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
